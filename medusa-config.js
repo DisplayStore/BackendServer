e175e527fc40e66c17628a06858ba2d3dc1cbcde
Arnav K <arnavkaushal09@gmail.com>
2024-01-10 17:44:14 +0000

    base

--- medusa-config.js
+++ medusa-config.js
@@ -1,32 +1,33 @@
 const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = "";
-switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
-  default:
-    ENV_FILE_NAME = ".env";
-    break;
-}
+let ENV_FILE_NAME = ".env";
+
+// switch (process.env.NODE_ENV) {
+//   case "production":
+//     ENV_FILE_NAME = ".env.production";
+//     break;
+//   case "staging":
+//     ENV_FILE_NAME = ".env.staging";
+//     break;
+//   case "test":
+//     ENV_FILE_NAME = ".env.test";
+//     break;
+//   case "development":
+//   default:
+//     ENV_FILE_NAME = ".env";
+//     break;
+// }
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+  console.log(process.env)
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "/vercel\\.app$/";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "/vercel\\.app$/";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
@@ -46,6 +47,7 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
+      serve: true,
       autoRebuild: true,
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
@@ -73,8 +75,8 @@ const modules = {
 const projectConfig = {
   jwtSecret: process.env.JWT_SECRET,
   cookieSecret: process.env.COOKIE_SECRET,
-  store_cors: STORE_CORS,
   database_url: DATABASE_URL,
+  store_cors:  STORE_CORS,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
   // redis_url: REDIS_URL
--- src/api/store/custom/route.ts
+++ src/api/store/custom/route.ts
@@ -4,5 +4,7 @@ export async function GET(
   req: MedusaRequest,
   res: MedusaResponse
 ): Promise<void> {
+  console.log(req,res)
+  res.setHeader('Access-Control-Allow-Origin', 'https://developer.mozilla.org')
   res.sendStatus(200);
 }
